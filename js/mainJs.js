48a356227f18d2cc0b73708ea096f56705ddf4b2
Savraj Singh <savrajsingh@gmail.com>
2019-01-16 09:24:41 -0500

    Doneish

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
 
+    $('a.scroll').click(function(){
+        $('html, body').animate({
+        scrollTop: $('#about').offset().top - 20}, 'slow');
+    });
+
+    $('a.resume').click(function(){
+        $('html, body').animate({
+        scrollTop: $('#resume').offset().top - 20}, 'slow');
+    });
+
+    $('a.contact').click(function(){
+        $('html, body').animate({
+        scrollTop: $('#contact').offset().top - 20}, 'slow');
+    });
 })
